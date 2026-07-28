7294f8d66c1bb662fc32df01265747070bc2a5d0
themeetkapuriya-sketch <themeetkapuriya-sketch@users.noreply.github.com>
2026-07-28 13:18:35 +0530

    Fix ProfileController view path to pages.profile and restore pages.inventory

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -16,7 +16,7 @@ class ProfileController extends Controller
      */
     public function profile()
     {
-        return view('profile.index');
+        return view('pages.profile');
     }
 
     /**
